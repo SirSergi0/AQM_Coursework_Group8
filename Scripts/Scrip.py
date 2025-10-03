db6376218fbcf14e669f3b54da7f584097443411
Sergio Castañeiras Morales <sergiocastaneirasmorales@gmail.com>
2025-10-03 11:09:45 +0200

    Shit happens

--- Scripts/Scrip.py
+++ Scripts/Scrip.py
@@ -2,7 +2,8 @@ from math import gcd
 from math import floor
 from random import randint
 
-n = 3451
+n = int(input("Enter de number you want to decompose (make sure it is not a prime number !!!): ",))
+# n=15
 
 def order(n,m):
     t = 1
@@ -11,7 +12,7 @@ def order(n,m):
     return int(t)
 
 while True:
-    m = randint(2,floor(n/7)-1)
+    m = randint(2,n-1)
     t=0
     g = gcd(n,m)
     if g != 1:
@@ -21,6 +22,17 @@ while True:
     else:
         r = order(n,m)
         if r%2 == 0:
-            m = pow(m,(r/2))+1
+            m = pow(m,int(r/2),n)+1
+            g = gcd(n,m)
+            if g != 1:
+                m=g
+                t=int(n/g)
+                break
+            m = pow(m,int(r/2),n)-1
+            g = gcd(n,m)
+            if g != 1:
+                m=g
+                t=int(n/g)
+                break
  
 print(n,"=",m,"*",t)
