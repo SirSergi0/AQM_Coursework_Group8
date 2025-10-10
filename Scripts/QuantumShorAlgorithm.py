95975b76fa7a63132fe175daa5c8056d7b1cfe4b
Sergio Castañeiras Morales <sergiocastaneirasmorales@gmail.com>
2025-10-10 22:36:55 +0200

    Plots happens

--- Scripts/QuantumShorAlgorithm.py
+++ Scripts/QuantumShorAlgorithm.py
@@ -9,6 +9,9 @@ from qiskit import QuantumCircuit, transpile
 from qiskit_aer import Aer
 from qiskit.visualization import plot_histogram
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+from collections import Counter
+import matplot2tikz
 
 np.set_printoptions(threshold=sys.maxsize)
 
@@ -37,6 +40,13 @@ def qft_dagger(n):
     qc.name = "QFT†"
     return qc
 
+
+def to_fraction(x, pos):
+    # Convert to a simplified fraction with denominator up to 128
+    f = Fraction(x).limit_denominator(128)
+    return rf'${f.numerator}/{f.denominator}$' if f.denominator != 1 else rf'${f.numerator}$'
+
+
 for i in tqdm(range (4,17), "Shit happens"):
     x       = 2
     N       = 55
@@ -55,11 +65,33 @@ for i in tqdm(range (4,17), "Shit happens"):
     t_qc          = transpile(Q, aer_sim)
     results       = aer_sim.run(t_qc).result()
     counts        = results.get_counts()
-    countsDecimal = {Fraction(int(k, 2)/(2**n_count)).limit_denominator(N): v for k, v in counts.items()}
+    for j in range(2 ** n_count):
+        key = format(j, f'0{n_count}b') 
+        if key not in counts:
+            counts[key] = 0
+    r_guess = []
+    countsNumeric = {int(k, 2)/(2**n_count): v for k, v in counts.items()}
+    x_UwU = sorted(countsNumeric.keys())
+    y = [countsNumeric[k] for k in x_UwU]
+    s = sum(y)
+    for k in range(len(y)):
+        y[k]= y[k]/s
 
-    plot_histogram(counts, title = f"N = {N}; n_count = {n_count}")
-    # plt.show()
-    plt.savefig(f"../Plots/FractionsPlotN{N}n_count{n_count}.jpg",dpi=300,bbox_inches='tight')
+
+    # plt.figure(figsize=(10,5))
+    plt.bar(x_UwU, y, width=0.005, align='center')  # width ~ spacing between your x values
+    plt.xlabel(r"$\frac{d}{r}$", fontsize=16)
+    plt.ylabel("Relative frequency")
+    
+    plt.gca().xaxis.set_major_formatter(FuncFormatter(to_fraction))
+    plt.tight_layout()
+    
+    # plt.hist(countsNumeric, bins = 1, range=(0, 2**n_count))
+
+    # plot_histogram(countsNumeric,  title = f"N = {N}; n_count = {n_count}")
+    plt.savefig(f"../Plots/FractionsPlotBinaryN{N}n_count{n_count}.jpg",dpi=300,bbox_inches='tight')
+    matplot2tikz.save(f"../Plots/FractionsPlotBinaryN{N}n_count{n_count}..tex")
+    plt.close()
 
     rows, measured_phases = [], []
     for output in counts:
@@ -71,9 +103,33 @@ for i in tqdm(range (4,17), "Shit happens"):
         # rows.append([f"{output}(bin) = {decimal:>3}(dec)", f"{decimal}/{2**n_count} = {phase:.2f}",f"{frac.numerator}/{frac.denominator}", frac.denominator, np.gcd(x**(frac.denominator//2)-1, N), np.gcd(x**(frac.denominator//2)+1, N)])
         if (np.gcd(x**(frac.denominator//2)-1, N) not in [1,N]) or (np.gcd(x**(frac.denominator//2)+1, N) not in [1,N]): # Getting rid of trivial divisors,
             rows.append([f"{output}(bin) = {decimal:>3}(dec)", f"{frac.numerator}/{frac.denominator}", frac.denominator, np.gcd(x**(frac.denominator//2)-1, N), np.gcd(x**(frac.denominator//2)+1, N)])
+            r_guess.append(frac.denominator)
     # Print the rows in a table
     # headers=["Register Output", "Phase", "Fraction", "Guess for r", "guess1","guess2"]
     headers = ["Register Output", "Fraction", "Guess for r", "guess1","guess2"]
     df      = pd.DataFrame(rows, columns=headers)
 
     df.to_csv(f'../Plots/FractionsPlotN{N}n_count{n_count}.csv', index=False)
+    
+    # Count occurrences of each unique value
+    counting = Counter(r_guess)
+
+    # Sort the values for nice x ordering
+    x_OuO = sorted(counting.keys())
+    y_OuO = [counting[k] for k in x_OuO]
+
+    # Plot as a centered bar chart
+    # plt.figure(figsize=(6,4))
+    plt.bar(x_OuO, y_OuO, width=2, align='center')
+
+    # Labels and style
+    plt.xlabel(r"$r_{Guess}$", fontsize=16)
+    plt.ylabel("Frequency")
+    plt.xticks(x_OuO)  # Show only existing integer values (4, 8, 16)
+
+    plt.savefig(f"../Plots/RGuessN{N}n_count{n_count}.jpg",dpi=300,bbox_inches='tight')
+    matplot2tikz.save(f"../Plots/RGuessN{N}n_count{n_count}.tex")
+
+    plt.close()
+
+
